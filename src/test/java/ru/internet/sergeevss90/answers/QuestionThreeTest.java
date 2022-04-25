166c1c890ff9f9e3954327c924d8ea571174f837
SergeevSergeiS <sergeevss90@internet.ru>
2022-04-25 21:17:22 +0400

    Added third answer

---
+++ src/test/java/ru/internet/sergeevss90/answers/QuestionThreeTest.java
@@ -0,0 +1,29 @@
+package ru.internet.sergeevss90.answers;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class QuestionThreeTest {
+    SelenideElement bi = $("#column-b");
+
+    @BeforeAll
+    static void prepare() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://the-internet.herokuapp.com/drag_and_drop";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void dragAndDropTest() {
+        open("");
+        $("#column-a").dragAndDropTo(bi);
+        bi.shouldHave(text("A"));
+    }
+}
+
